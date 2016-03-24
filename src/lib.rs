b4c4b70283d30fd8ca2b53c550360bf512dde7d8
Wangshan Lu <wisagan@gmail.com>
2016-03-24 19:13:07 +0800

    Rename to simple-error
    string_error gives the false impression the error is about string type.
    Use simple error to better represent the type. Kebab-case is
    aesthetically more pleasing than snake-case.

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,30 @@
 use std::fmt;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct StringError {
+pub struct SimpleError {
     err: String,
 }
 
-impl StringError {
-    pub fn new<T: Into<String>>(t: T) -> StringError {
-        StringError{ err: t.into() }
+impl SimpleError {
+    pub fn new<T: Into<String>>(t: T) -> SimpleError {
+        SimpleError{ err: t.into() }
     }
 
-    pub fn from<T: std::error::Error>(t: T) -> StringError {
-        StringError{ err: format!("{}", t) }
+    pub fn from<T: std::error::Error>(t: T) -> SimpleError {
+        SimpleError{ err: format!("{}", t) }
     }
 }
 
 // TODO: implement From<T> where T: std::error::Error when specialization lands, and remove
 // inherent from function.
 
-impl fmt::Display for StringError {
+impl fmt::Display for SimpleError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         self.err.fmt(f)
     }
 }
 
-impl std::error::Error for StringError {
+impl std::error::Error for SimpleError {
     fn description(&self) -> &str {
         &self.err
     }
@@ -32,26 +32,26 @@ impl std::error::Error for StringError {
 
 #[cfg(test)]
 mod tests {
-    use super::StringError;
+    use super::SimpleError;
     use std::error::Error;
     use std::io;
 
     #[test]
     fn new_from_string() {
-        let err = StringError::new(String::from("an error from String"));
+        let err = SimpleError::new(String::from("an error from String"));
         assert_eq!("an error from String", format!("{}", err));
         assert_eq!("an error from String", err.description());
     }
 
     #[test]
     fn new_from_str() {
-        let err = StringError::new("an error from str");
+        let err = SimpleError::new("an error from str");
         assert_eq!("an error from str", format!("{}", err));
     }
 
     #[test]
     fn from_io_error() {
-        let err = StringError::from(io::Error::new(io::ErrorKind::Other, "oh no"));
+        let err = SimpleError::from(io::Error::new(io::ErrorKind::Other, "oh no"));
         assert_eq!("oh no", format!("{}", err));
     }
 }
