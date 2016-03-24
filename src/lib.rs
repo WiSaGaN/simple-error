a1242a3e515fe9568ba951ca82c7ae672bb2c1c7
Wangshan Lu <wisagan@gmail.com>
2016-03-24 22:53:51 +0800

    Complete docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![doc(html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "https://www.rust-lang.org/favicon.ico",
        html_root_url = "https://wisagan.github.io/simple-error/simple_error/")]
+#![deny(missing_docs)]
 //! A simple error type backed by a string.
 //!
 //! This crate provides a `SimpleError` type, which implements `std::error::Error`. The underlying
@@ -8,20 +9,53 @@
 //!
 //! It should be used when all you care about is an error string.
 //!
-//! It should not be used when you want to programmatically handle the error.
+//! It should not be used when you want to programmatically handle different kinds of an error.
 
 use std::fmt;
 
+/// A type that represents a simple error.
+///
+/// This type uses a `String` to store the error string, and it implements `std::error::Error`.
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct SimpleError {
     err: String,
 }
 
 impl SimpleError {
+    /// Creates a new simple error.
+    ///
+    /// This function can take any type that implements `Into<String>`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use self::simple_error::SimpleError;
+    ///
+    /// // errors can be created from `str`
+    /// let err = SimpleError::new("an error from str");
+    ///
+    /// // errors can also be created from `String`
+    /// let err = SimpleError::new(String::from("an error from String"));
+    /// ```
     pub fn new<T: Into<String>>(t: T) -> SimpleError {
         SimpleError{ err: t.into() }
     }
 
+    /// Creates a new simple error from another error.
+    ///
+    /// This function can take any type that implements `std::error::Error`.
+    /// The error string will be the `Display` of the `std::error::Error`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use self::simple_error::SimpleError;
+    /// use std::io;
+    ///
+    /// // errors can be created from `io::Error`
+    /// let err = SimpleError::from(io::Error::new(io::ErrorKind::Other, "oh no"));
+    /// assert_eq!("oh no", format!("{}", err));
+    /// ```
     pub fn from<T: std::error::Error>(t: T) -> SimpleError {
         SimpleError{ err: format!("{}", t) }
     }
